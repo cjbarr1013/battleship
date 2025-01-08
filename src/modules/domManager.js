8b8f129b351bc703bda0845a2e4a9777606d09b0
Cameron Barr <cjbarr1013@gmail.com>
2025-01-08 13:07:14 -0500

    temp commit

---
+++ src/modules/domManager.js
@@ -0,0 +1,18 @@
+export function DOMManager() {
+  const fillSquare = (status) => {
+    switch (status) {
+      case 'hit':
+        // apply hit class
+        break;
+      case 'miss':
+        // apply miss class
+        break;
+      case 'unharmed':
+        // apply unharmed class
+        break;
+      case 'empty':
+        // apply empty class
+        break;
+    }
+  };
+}
