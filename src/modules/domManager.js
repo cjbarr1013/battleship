52389a95f5b7b3c2d523adce2708b35b3c76b081
Cameron Barr <cjbarr1013@gmail.com>
2025-01-13 23:31:57 -0500

    Add function for creating game grid

--- src/modules/domManager.js
+++ src/modules/domManager.js
@@ -1,4 +1,23 @@
 export function DOMManager() {
+  const initGameboards = () => {
+    const player1Board = [document.querySelector('#player-1 .board'), 'p1'];
+    const player2Board = [document.querySelector('#player-2 .board'), 'p2'];
+    [player1Board, player2Board].forEach((board) => {
+      createGameboard(board);
+    });
+  };
+
+  const createGameboard = (boardInfo) => {
+    const yLabels = 'ABCDEFGHIJ';
+    for (let y = 0; y < yLabels.length; y++) {
+      for (let x = 1; x <= 10; x++) {
+        const square = document.createElement('div');
+        square.id = `${boardInfo[1]}-${x},${yLabels[y]}`;
+        boardInfo[0].appendChild(square);
+      }
+    }
+  };
+
   const fillSquare = (status) => {
     switch (status) {
       case 'hit':
@@ -14,5 +33,9 @@ export function DOMManager() {
         // apply empty class
         break;
     }
+  };
+
+  return {
+    initGameboards,
   };
 }
