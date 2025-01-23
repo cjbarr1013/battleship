cb2abfa7da590d6c00ffae136c985bbc1337abf9
Cameron Barr <cjbarr1013@gmail.com>
2025-01-23 16:44:34 -0500

    Change filename from gameplay to controller.js

---
+++ src/modules/controller.js
@@ -0,0 +1,8 @@
+import { Player } from './player.js';
+
+export function PregameController() {}
+
+export function IngameController() {
+  const player1 = Player('Player 1', 'p1');
+  const computer = Player('Computer', 'p2');
+}
